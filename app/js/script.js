9e9c28d3e0f4766af770f1055577cc6c175b662e
jgustavoj <gjime011@fiu.edu>
2021-01-21 18:06:05 -0500

    overlay done

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,21 @@
-//This function adds/removes "open" class into hamburger menu on Click
+//This function adds/removes "open" class into header menu on Click
+//This was done in order to work with the overaly better
+const header = document.querySelector(".header");
 
+//This function adds/removes "open" class into hamburger menu on Click
 const btnHamburger = document.querySelector("#btnHamburger");
+
+const overlay = document.querySelector(".overlay");
+
 btnHamburger.addEventListener("click", () => {
-  console.log("click hamburger");
-  btnHamburger.classList.contains("open")
-    ? btnHamburger.classList.remove("open")
-    : btnHamburger.classList.add("open");
+  console.log("clicked!");
+  if (header.classList.contains("open")) {
+    header.classList.remove("open"); // Close hamburger memu
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  } else {
+    header.classList.add("open"); // Open hamburger menu
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+  }
 });
