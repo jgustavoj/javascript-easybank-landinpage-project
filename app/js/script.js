222ae9487c89d53026f6a7ac0aa6defb6ebace89
jgustavoj <gjime011@fiu.edu>
2021-01-21 19:12:43 -0500

    overlay menu done

--- app/js/script.js
+++ app/js/script.js
@@ -1,21 +1,28 @@
-//This function adds/removes "open" class into header menu on Click
-//This was done in order to work with the overaly better
-const header = document.querySelector(".header");
-
 //This function adds/removes "open" class into hamburger menu on Click
 const btnHamburger = document.querySelector("#btnHamburger");
-
+//This function adds/removes "open" class into header menu on Click
+//This was done in order to work with the overaly better
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", () => {
   console.log("clicked!");
+
   if (header.classList.contains("open")) {
-    header.classList.remove("open"); // Close hamburger memu
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    body.classList.remove("noscroll");
+    header.classList.remove("open"); // Close hamburger meny
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
   } else {
+    body.classList.add("noscroll");
     header.classList.add("open"); // Open hamburger menu
-    overlay.classList.remove("fade-out");
-    overlay.classList.add("fade-in");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
   }
 });
